dfd6655013df511e699e7e1e1b862e2d3b778fbb
BuiLongCuong <builongcuong76@gmail.com>
2024-01-11 21:09:04 +0700

    lại fix lại giao diện đn đk

--- src/pages/products/AddProduct/AddProduct.js
+++ src/pages/products/AddProduct/AddProduct.js
@@ -45,153 +45,50 @@ function AddProduct() {
     return (
         <>
             <h1> ADD PRODUCT </h1>
-            {/*<Formik initialValues={{*/}
-            {/*    productName: '',*/}
-            {/*    description: '',*/}
-            {/*    price: '',*/}
-            {/*    stockQuantity: '',*/}
-            {/*    category: {*/}
-            {/*        id: ""*/}
-            {/*    },*/}
-            {/*    account: {*/}
-            {/*        id: accountSupplier.id*/}
-            {/*    }*/}
+            <Formik initialValues={{
+                productName: '',
+                description: '',
+                price: '',
+                stockQuantity: '',
+                category: {
+                    id: ""
+                },
+                account: {
+                    id: accountSupplier.id
+                }
 
-            {/*}}*/}
+            }}
 
-            {/*        onSubmit={Create}*/}
+                    onSubmit={Create}
 
-            {/*>*/}
-            {/*    <Form>*/}
-            {/*        <Field name={"productName"} placeholder={"Enter ProductName"}/>*/}
-            {/*        <Field name={"description"} placeholder={"Enter Description"}/>*/}
-            {/*        <Field name={"price"} placeholder={" Enter Price"}/>*/}
-            {/*        <Field name={"stockQuantity"} placeholder={" Enter StockQuantity"}/>*/}
-            {/*        <Field name={"category.id"} as={"select"}>*/}
-            {/*            {*/}
-            {/*                categories.map((category) => {*/}
-            {/*                    return <>*/}
-            {/*                        <option value={category.id}>{category.name}</option>*/}
-            {/*                    </>*/}
-            {/*                })*/}
-            {/*            }*/}
-            {/*        </Field>*/}
-            {/*        <Field name={"photo.photoName"} type={"file"} multiple onChange={handleChange}*/}
-            {/*               placeholder={" Enter Photo"}/>*/}
-            {/*        {*/}
-            {/*            photo.map(p => (*/}
-            {/*                <>*/}
-            {/*                    <img src={p.photoName} alt=""/>*/}
-            {/*                </>*/}
-            {/*            ))*/}
-            {/*        }*/}
-            {/*        <Field name={"account"} type={"hidden"} placeholder={" Enter Supplier"}/>*/}
-            {/*        <button type={"submit"}>Save</button>*/}
-            {/*    </Form>*/}
-            {/*</Formik>*/}
-
-            <div className="mainAddPr">
-                <div className="headerAddPr">
-                    <div className="headerDetailAddPr">
-                        <div className="leftHeader">
-                            <div className="logoHeader">
-                                <img src="/images/img_8.png"
-                                     alt=""/>
-                            </div>
-                            <div className="titleAdd">
-                                <p>Thêm mới sản phẩm</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="frame">
-                    <Formik initialValues={
+            >
+                <Form>
+                    <Field name={"productName"} placeholder={"Enter ProductName"}/>
+                    <Field name={"description"} placeholder={"Enter Description"}/>
+                    <Field name={"price"} placeholder={" Enter Price"}/>
+                    <Field name={"stockQuantity"} placeholder={" Enter StockQuantity"}/>
+                    <Field name={"category.id"} as={"select"}>
                         {
-                            productName: '',
-                            description: '',
-                            price: '',
-                            stockQuantity: '',
-                            category: {
-                                id: ''
-                            },
-                            account: {
-                                id: accountSupplier.id
-                            }
-
+                            categories.map((category) => {
+                                return <>
+                                    <option value={category.id}>{category.name}</option>
+                                </>
+                            })
                         }
-                    } onSubmit={Create}
-                    >
-                        <Form>
-                            <div className="image">
-                                <Field name={"photo.photoName"} type={"file"} multiple onChange={handleChange}
-                                       placeholder={" Enter Photo"}/>
-                                {
-                                    photo.map(p => (
-                                        <>
-                                            <img src={p.photoName} alt="" style={{width: "150px", height: "150px"}}/>
-                                        </>
-                                    ))
-                                }
-                            </div>
-                            <div className="infoProduct">
-                                <div className="contentProduct">
-                                    <div className="nameProduct">
-                                        <div className="label1">
-                                            Nhập tên :
-                                        </div>
-                                        <div className="nameDetail">
-                                            <Field name={"productName"} placeholder={"Enter ProductName"}/>
-                                        </div>
-                                    </div>
-                                    <div className="descriptionProduct">
-                                        <div className="label2">
-                                            Mô tả :
-                                        </div>
-                                        <div className="descriptionDetail">
-                                            <Field as="textarea" cols={39} rows={4} name={"description"}
-                                                   placeholder={"Enter Description"}/>
-                                        </div>
-                                    </div>
-                                    <div className="priceProduct">
-                                        <div className="label3">
-                                            Giá (VNĐ) :
-                                        </div>
-                                        <div className="priceDetail">
-                                            <Field name={"price"} type={"number"} placeholder={" Enter Price"}/>
-                                        </div>
-                                    </div>
-                                    <div className="quantityProduct">
-                                        <div className="label4">
-                                            Số lượng :
-                                        </div>
-                                        <div className="quantityDetail">
-                                            <Field name={"stockQuantity"} type={"number"}
-                                                   placeholder={" Enter StockQuantity"}/>
-                                        </div>
-                                    </div>
-                                    <div className="categoryProduct">
-                                        <div className="label5">
-                                            Loại sản phẩm :
-                                        </div>
-                                        <Field name={"category.id"} as={"select"}>
-                                            {
-                                                categories.map((category) => {
-                                                    return <>
-                                                        <option value={category.id}>{category.name}</option>
-                                                    </>
-                                                })
-                                            }
-                                        </Field>
-                                    </div>
-                                    <div className="addProduct">
-                                        <button type={"submit"}>Thêm</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </Form>
-                    </Formik>
-                </div>
-            </div>
+                    </Field>
+                    <Field name={"photo.photoName"} type={"file"} multiple onChange={handleChange}
+                           placeholder={" Enter Photo"}/>
+                    {
+                        photo.map(p => (
+                            <>
+                                <img src={p.photoName} alt=""/>
+                            </>
+                        ))
+                    }
+                    <Field name={"account"} type={"hidden"} placeholder={" Enter Supplier"}/>
+                    <button type={"submit"}>Save</button>
+                </Form>
+            </Formik>
         </>
     )
 }
