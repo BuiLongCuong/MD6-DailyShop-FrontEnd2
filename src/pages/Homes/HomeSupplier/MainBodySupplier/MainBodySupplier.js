4c0e67896333180619ce16c4c579a0d046ba500b
BuiLongCuong <builongcuong76@gmail.com>
2024-01-12 14:23:46 +0700

    merge từ nhánh DetailProduct

--- src/pages/Homes/HomeSupplier/MainBodySupplier/MainBodySupplier.js
+++ src/pages/Homes/HomeSupplier/MainBodySupplier/MainBodySupplier.js
@@ -15,8 +15,6 @@ export default function MainBodySupplier(){
                            + Thêm mới sản phẩm
                         </button>
                     </Link>
-
-
             </div>
         </>
     )
