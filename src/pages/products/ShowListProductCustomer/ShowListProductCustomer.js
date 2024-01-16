3e844b09ee07185bd5437618e3da557984e49713
Huanphamvan <phamvanhuan.bk@gmail.com>
2024-01-16 16:10:25 +0900

    DetailProduct

--- src/pages/products/ShowListProductCustomer/ShowListProductCustomer.js
+++ src/pages/products/ShowListProductCustomer/ShowListProductCustomer.js
@@ -17,26 +17,52 @@ export default function ShowListProductCustomer(){
         <>
 
             <div className="show-list-product-customer">
-                {listProducts && listProducts.map((products) => (
-                        <>  <Link to={"#"}>
-                            <div className="card-detail-show-product">
-                                <div className="card-detail-show-product-image">
-                                    <img
-                                        src={products.photo[0].photoName}
-                                        alt=""/>
-                                </div>
-                                <div className="card-detail-show-product-name">
-                                    {products.category.name}
-                                </div>
-                                <div className="card-detail-show-product-price">
-                                    $ {products.price}
-                                </div>
-                            </div>
-                        </Link>
+                <div className="card-detail-show-product-cover">
+                    <div className="card-detail-show-product">
+                        <div className="card-detail-show-product-image">
+                            <img src="https://cdn.tgdd.vn/Files/2017/05/23/985304/nhung-cong-dung-tuyet-voi-cua-thanh-long-ban-nen-biet-202109171157393465.jpg" alt=""/>
+                        </div>
+                        <div className="card-detail-show-product-info">
+                            <div className="card-detail-show-product-name"></div>
+                            <div className="card-detail-show-product-blank"></div>
+                            <div className="card-detail-show-product-price">
+                        </div>
 
-                        </>
-                    )
-                )}
+
+                        </div>
+                    </div>
+                </div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+                <div className="card-detail-show-product-cover"></div>
+
+                {/*{listProducts && listProducts.map((products) => (*/}
+                {/*        <> */}
+                {/*            <Link to={"#"}>*/}
+                {/*            <div className="card-detail-show-product">*/}
+                {/*                <div className="card-detail-show-product-image">*/}
+                {/*                    <img*/}
+                {/*                        src={products.photo[0].photoName}*/}
+                {/*                        alt=""/>*/}
+                {/*                </div>*/}
+                {/*                <div className="card-detail-show-product-name">*/}
+                {/*                    {products.category.name}*/}
+                {/*                </div>*/}
+                {/*                <div className="card-detail-show-product-price">*/}
+                {/*                    $ {products.price}*/}
+                {/*                </div>*/}
+                {/*            </div>*/}
+                {/*        </Link>*/}
+                {/*        </>*/}
+                {/*    )*/}
+                {/*)}*/}
 
             </div>
         </>
