a3ca7b6995f8c83a930c2e2f1415fa6c870721a0
BuiLongCuong <builongcuong76@gmail.com>
2024-01-17 14:21:10 +0700

    merge từ nhánh cuong

--- src/redux/service/axios/getAxios.js
+++ src/redux/service/axios/getAxios.js
@@ -1,30 +1,30 @@
 import axios, {get} from "axios";
 
 export function getAxios() {
-    const currentSupplier = JSON.parse(localStorage.getItem("currentSupplier"));
-    const currentUser = JSON.parse(localStorage.getItem("currentSupplier"));
-    const currentCustomer = JSON.parse(localStorage.getItem("currentCustomer"));
-    if (currentUser){
-        return axios.create({
-            baseURL: "http://localhost:8088",
-            headers:{ Authorization:`Bearer ${currentUser.accessToken}`}
-        })
-    }else if (currentSupplier) {
-        return axios.create({
-            baseURL: "http://localhost:8088",
-            headers: {"Authorization": `Bearer ${currentSupplier.accessToken}`}
-        })
-    }else if (currentCustomer) {
-        return axios.create({
-            baseURL: "http://localhost:8088",
-            headers: {"Authorization": `Bearer ${currentCustomer.accessToken}`}
-        })
-    }
-    else {
+    // const currentSupplier = JSON.parse(localStorage.getItem("currentSupplier"));
+    // const currentUser = JSON.parse(localStorage.getItem("currentSupplier"));
+    // const currentCustomer = JSON.parse(localStorage.getItem("currentCustomer"));
+    // if (currentUser){
+    //     return axios.create({
+    //         baseURL: "http://localhost:8088",
+    //         headers:{ Authorization:`Bearer ${currentUser.accessToken}`}
+    //     })
+    // }else if (currentSupplier) {
+    //     return axios.create({
+    //         baseURL: "http://localhost:8088",
+    //         headers: {"Authorization": `Bearer ${currentSupplier.accessToken}`}
+    //     })
+    // }else if (currentCustomer) {
+    //     return axios.create({
+    //         baseURL: "http://localhost:8088",
+    //         headers: {"Authorization": `Bearer ${currentCustomer.accessToken}`}
+    //     })
+    // }
+    // else {
         return axios.create({
             baseURL: "http://localhost:8088"
         })
-    }
+    // }
 }
 
 export const getCustomerUrl = () => {
