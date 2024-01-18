ecf6d96f28ff2ef704c75187c26b5bac8cfc4259
BuiLongCuong <builongcuong76@gmail.com>
2024-01-18 12:05:42 +0700

    okok

--- src/redux/service/oderService.js
+++ src/redux/service/oderService.js
@@ -4,7 +4,7 @@ import {getAxios, getCustomerUrl} from "./axios/getAxios";
 export const addProductToOrders = createAsyncThunk(
     'order/addProductToOrders',
     async (data) => {
-        let res = await getCustomerUrl().get("/customer/addProductToOrders", data);
+        let res = await getCustomerUrl().post("/customer/addProductToOrders", data);
         return res.data;
     }
 )
