5d2d5e61872bcbfba80f7ad8e39fd67c5399b101
Huanphamvan <phamvanhuan.bk@gmail.com>
2024-01-19 12:13:31 +0900

    RePagination

--- src/pages/products/ShowListProductCustomerWithoutLogin/ShowListProductCustomerWithoutLogin.js
+++ src/pages/products/ShowListProductCustomerWithoutLogin/ShowListProductCustomerWithoutLogin.js
@@ -6,58 +6,60 @@ import {Link, useParams, useSearchParams} from "react-router-dom";
 import {forEach} from "react-bootstrap/ElementChildren";
 import {Pagination} from "./pagination/Pagination";
 import {current} from "@reduxjs/toolkit";
-export default function ShowListProductCustomerWithoutLogin(){
+
+export default function ShowListProductCustomerWithoutLogin() {
     const dispatch = useDispatch();
 
     const [searchParam] = useSearchParams()
-    const [page,setPage]=useState(+searchParam.get('page'))
+    const [page, setPage] = useState(+searchParam.get('page'))
 
-    const changePage=(newPage)=>{
+    const changePage = (newPage) => {
         setPage(newPage)
     }
 
     const listProducts = useSelector(({products}) => {
         return products.list
     })
-    const totalPages =useSelector(({products} )=>{
+    const totalPages = useSelector(({products}) => {
         return products.totalPages;
     })
     useEffect(() => {
         dispatch(getAllProductWithoutLogin(page))
     }, [page]);
-    return(
+    return (
         <>
             <div className="show-list-product-customer-without-login">
                 {listProducts && listProducts.map((products) => (
                         <>
                             {/*<Link to={"/customer/products/detail/" + products.productID}>*/}
-                                <Link to={"/customer/products/detail/" + products.productID} className="card-detail-show-product-cover-without-login">
-                                    <div className="card-detail-show-product-without-login">
-                                        <div className="card-detail-show-product-image-without-login">
-                                            <img
-                                                src={products.photo.length>0 ? products.photo[0].photoName: ""}
-                                                alt=""/>
-                                        </div>
-                                        <div className="card-detail-show-product-info-without-login">
-                                            <div className="card-detail-show-product-name-without-login">
-                                                {products.productName}, {products.category.name}
-                                            </div>
-                                            <div className="card-detail-show-product-blank-without-login">
-
-                                            </div>
-                                            <div className="card-detail-show-product-price-without-login">
-                                                <span style={{color: "red"}}>Giá : $ {products.price}</span>
-                                            </div>
-
-
-                                        </div>
+                            <Link to={"/customer/products/detail/" + products.productID}
+                                  className="card-detail-show-product-cover-without-login">
+                                <div className="card-detail-show-product-without-login">
+                                    <div className="card-detail-show-product-image-without-login">
+                                        <img
+                                            src={products.photo.length > 0 ? products.photo[0].photoName : ""}
+                                            alt=""/>
                                     </div>
-                                </Link>
+                                    <div className="card-detail-show-product-info-without-login">
+                                        <div className="card-detail-show-product-name-without-login">
+                                            {products.productName}, {products.category.name}
+                                        </div>
+                                        <div className="card-detail-show-product-blank-without-login">
+
+                                        </div>
+                                        <div className="card-detail-show-product-price-without-login">
+                                            <span style={{color: "red"}}>Giá : $ {products.price}</span>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            </Link>
                             {/*</Link>*/}
                         </>
                     )
                 )}
-                {page || totalPages? <Pagination total={totalPages} current={page} changePage={changePage}/>:""}
+                {page || totalPages ? <Pagination total={totalPages} current={page} changePage={changePage}/> : ""}
             </div>
 
 
